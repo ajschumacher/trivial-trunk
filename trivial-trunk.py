722a1a3e3b98a4eb6e64e8b5a6ac0bdf1d97d504
Aaron Schumacher <ajschumacher@gmail.com>
2012-06-03 19:39:20 -0400

    works but doesn't tell you anything

--- trivial-trunk.py
+++ trivial-trunk.py
@@ -1,27 +1,23 @@
-import webapp2
-import json
-from google.appengine.api import urlfetch
-import os
+import webapp2, json, os
+from google.appengine.ext import db
 from google.appengine.ext.webapp import template
+
+class TrivialInteraction(db.Model):
+    score = db.IntegerProperty()
+    date = db.DateTimeProperty(auto_now_add=True)
 
 class RootResponse(webapp2.RequestHandler):
     def get(self):
-        response_data = {'score': 1}
-        i_data = self.request.get_all('i')
-        try:
-            response_data['i'] = i_data[0]
-        except:
-            pass
-        data_urls = self.request.get_all('d')
-        try:
-            urlfetch.fetch(url = data_urls[0],
-                           payload = json.dumps(response_data),
-                           method = urlfetch.PUT)
-        except:
-            pass
-        template_values = {}
-        path = os.path.join(os.path.dirname(__file__), 'leaf.html')
+        interaction = TrivialInteraction()
+        interaction.put()
+        template_values = { 'i': str(interaction.key()) }
+        path = os.path.join(os.path.dirname(__file__), 'trunk.html')
         self.response.out.write(template.render(path, template_values))
+    def put(self):
+        data = json.loads(self.request.body)
+        interaction = db.get(data['i'])
+        interaction.score = data['score']
+        interaction.put()
 
 app = webapp2.WSGIApplication([('/', RootResponse)],
                               debug=True)
